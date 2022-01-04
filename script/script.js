a9f01d037655217dde1f4dfe2b9c82e86f5c28d9
Baiel Muzuraimov <84644662+bmuzuraimov@users.noreply.github.com>
2022-01-04 14:11:02 +0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,47 +1,77 @@
-var video_boundaries = $("#streaming-video")[0].getBoundingClientRect();//get coordinates of the video
-var cursor_pos = { x: -1, y: -1 };//cursor coordinates relatively to the video frame
-var last_line = $('svg line').length-1;//length of the last line in svg
-var active_line = true;//line follow cursor
-var first_point = true;//remember first coordinates to check if distance with last point is close
-// Size of browser viewport.
-var width = window.screen.height;//width of browser's viewport
-var height = window.screen.width;//height of browser's viewport
-var region_coordinates = [];//region coordinates
+var vid_bound = $('#streaming-video')[0].getBoundingClientRect();//get coordinates of the video
+var m_pos = { x: -1, y: -1 };//cursor coordinates relatively to the video frame
+var l_line = $('svg line').length-1;//index of the last line
+var a_line = true;//is line follow mouse
+var f_point = true;//remember first point to check if distance with last point is close
+var regions = new Array();//all regions
+var cur_region = new Array();//current region coordinates
 $('#streaming-video').mousemove(function(){
-    cursor_pos.x = event.pageX-video_boundaries.left;
-    cursor_pos.y = event.pageY-video_boundaries.top;
-    if(active_line){
-        $("svg line").eq(last_line).attr('x2',cursor_pos.x).attr('y2',cursor_pos.y);
-        $("svg circle").attr('cx', cursor_pos.x).attr('cy', cursor_pos.y);
-    }
-});
-
-$("#draw-region").change(function() {
-    if(this.checked) {
-        $("video").trigger('pause');
-        $('svg').removeClass('hidden');
-    }else{
-        $("video").trigger('play');
-        $('svg').addClass('hidden');
-    }
-});
-
-$("video").click(function() {
-    cursor_pos.x = event.pageX-video_boundaries.left;
-    cursor_pos.y = event.pageY-video_boundaries.top;
-    if(first_point){
-        first_point = false;
-        region_coordinates.push([cursor_pos.x,cursor_pos.y]);
-    }else{
-        let dist = Math.sqrt( Math.pow((cursor_pos.x-region_coordinates[0][0]), 2) + Math.pow((cursor_pos.y-region_coordinates[0][1]), 2) );
-        if (dist<10) {
-            active_line = false;
-            cursor_pos.x = init_cor.x;
-            cursor_pos.y = init_cor.y;
-        }else{
-            region_coordinates.push([cursor_pos.x,cursor_pos.y]);
+    m_pos.x = event.pageX-vid_bound.left;
+    m_pos.y = event.pageY-vid_bound.top;
+    if(a_line){
+        if (!f_point) {
+            $('svg line').eq(l_line).attr('x2',m_pos.x).attr('y2',m_pos.y);
         }
+        $('#cursor').attr('cx', m_pos.x).attr('cy', m_pos.y);
     }
-    console.log(region_coordinates);//shows coordinates of plotted dots
-    $(document.createElementNS('http://www.w3.org/2000/svg','line')).attr({x1:cursor_pos.x,y1:cursor_pos.y,x2:cursor_pos.x,y2:cursor_pos.y}).appendTo("svg");
 });
+$('#draw-btn').click(function() {
+    let mode = $('#draw-btn').text();
+    if (mode=='Set Count Region') {
+        $('#draw-btn').text('Cancel');
+        $('svg').removeClass('hidden');
+        $('.panel__video video').css({'cursor': 'crosshair'});
+    }else if (mode=='Cancel') {
+        $('#draw-btn').text('Set Count Region');
+        $('svg').addClass('hidden');
+        $('.panel__video video').css({'cursor': 'default'});
+    }else if (mode=='Reset' && regions.length == 0) {
+        $('#draw-btn').text('Cancel');
+        $('svg line:nth-last-of-type(-n+'+cur_region.length+')').remove();
+        cur_region = [];
+        f_point = true;
+    }else if (mode=='Reset' && regions.length > 0) {
+        $('#draw-btn').text('Save');
+        $('svg line:nth-last-of-type(-n+'+cur_region.length+')').remove();
+        cur_region = [];
+        f_point = true;
+    } else {
+        alert('Image Saved');
+        $('#draw-btn').text('Set Count Region');
+        a_line = true;
+    }
+});
+
+$('#streaming-video').click(function(){
+    let mode = $('#draw-btn').text();
+    if(mode!='Set Count Region'){
+        m_pos.x = event.pageX-vid_bound.left;
+        m_pos.y = event.pageY-vid_bound.top;
+        m_pos.x = m_pos.x.toFixed(2);
+        m_pos.y = m_pos.y.toFixed(2);
+        if(f_point){
+            $('#draw-btn').text('Reset');
+            f_point = false;
+            regions.push(cur_region);//create new region
+            regions[regions.length-1].push([m_pos.x,m_pos.y]);
+        }else{
+            let dist = Math.sqrt(Math.pow((m_pos.x-cur_region[0][0]), 2) + Math.pow((m_pos.y-cur_region[0][1]), 2));
+            if (dist<10) {
+                $('#draw-btn').text('Save');
+                $('svg line').eq(l_line).attr('x2',regions[regions.length-1][0][0]).attr('y2',regions[regions.length-1][0][1]);
+                $(document.createElementNS('http://www.w3.org/2000/svg','circle')).attr({cx:regions[regions.length-1][0][0],cy:regions[regions.length-1][0][1],r:2}).appendTo("svg");
+                $('svg line').css({'stroke-width': '4'});
+                $('svg circle').css({'stroke-width': '5'});
+                f_point = true;
+                cur_region = [];//empty current region array
+                let json_regions = JSON.stringify(regions);
+                console.log(json_regions);//shows coordinates of plotted dots
+                return false;
+            }else{
+                regions[regions.length-1].push([m_pos.x,m_pos.y]);
+            }
+        }
+        $(document.createElementNS('http://www.w3.org/2000/svg','line')).attr({x1:m_pos.x,y1:m_pos.y,x2:m_pos.x,y2:m_pos.y}).appendTo("svg");
+    }
+});
+
